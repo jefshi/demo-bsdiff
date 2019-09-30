bacd46fcc9508a6e0b58a30254144a5019201d85
jefshi <jefshi@yeah.net>
2019-09-30 11:19:53 +0800

    应用层调用 bspatch 合并增量包

--- app/src/main/java/com/demo/bsdiff/MainActivity.java
+++ app/src/main/java/com/demo/bsdiff/MainActivity.java
@@ -1,8 +1,13 @@
 package com.demo.bsdiff;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.Environment;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.TextView;
+
+import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,9 +21,30 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Example of a call to a native method
-        TextView tv = findViewById(R.id.sample_text);
-        tv.setText(stringFromJNI());
+        findViewById(R.id.sample_text).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ((TextView) view).setText(stringFromJNI());
+            }
+        });
+
+        findViewById(R.id.btn_permission).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ActivityCompat.requestPermissions(MainActivity.this, new String[]{android.Manifest.permission.WRITE_EXTERNAL_STORAGE}, 2);
+            }
+        });
+
+        findViewById(R.id.btn_bspatch).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final File old = new File(Environment.getExternalStorageDirectory(), "old.apk");
+                final File out = new File(Environment.getExternalStorageDirectory(), "out.apk");
+                final File patch = new File(Environment.getExternalStorageDirectory(), "PATCH.patch");
+                BsdiffUtil.bspatch(old, out, patch);
+            }
+        });
+
     }
 
     /**
